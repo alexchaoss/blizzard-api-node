f53dd06b5406611b6928c15cccd8b8d20de90e8d
Alexandre St-Pierre <alexchaoss@hotmail.com>
2023-12-04 15:40:22 -0500

    add metrics

--- server.ts
+++ server.ts
@@ -3,9 +3,18 @@ import Express from 'express';
 import cors from 'cors';
 import { initDB, closeDB } from './src/database';
 import { initEndpoints } from './src/endpoints';
+import swStats from 'swagger-stats';
+import { version } from './package.json';
 
 const server = Express();
 server.use(cors());
+server.use(swStats.getMiddleware({
+  version, uriPath: '/metrics',
+  authentication: true,
+  onAuthenticate: (req, username, password) => {
+    return process.env.USERNAME === username && process.env.PASSWORD === password;
+  }
+}));
 const port = process.env.PORT || 8000;
 
 initDB();
